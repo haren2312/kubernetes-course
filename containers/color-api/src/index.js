56ad4b9d7a7b51fc753d4bbf4deee4ef857e617c
Lauro Fialho Müller <contact@lauromueller.com>
2024-09-21 22:41:20 +0200

    feat(color-api): code v2.0.0

--- containers/color-api/src/index.js
+++ containers/color-api/src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const { healthRouter } = require('./routes/health');
 const { apiRouter } = require('./routes/api');
 const { rootRouter } = require('./routes/root');
 
-const app = express();
 const port = 80;
+
+const app = express();
 
 const delay_startup = process.env.DELAY_STARTUP === 'true';
 console.log(`Delay startup : ${delay_startup}`);
 
+app.use(bodyParser.json());
 app.use('/api', apiRouter);
 app.use('/', healthRouter);
 app.use('/', rootRouter);
@@ -21,6 +25,16 @@ if (delay_startup) {
   while (Date.now() - start < 60000) {}
 }
 
-app.listen(port, () => {
-  console.log(`Color API listening on port: ${port}`);
-});
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(port, () => {
+      console.log(`Color API listening on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Could not connect to MongoDB');
+    console.error(err);
+  });
--- containers/color-api/src/routes/api.js
+++ containers/color-api/src/routes/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { getColor, getHostname } = require('../utils');
+const { getHostname } = require('../utils');
+const { getColor, getColors, deleteColor, saveColor } = require('../db/color');
 
 const apiRouter = express.Router();
 
-apiRouter.get('/', (req, res) => {
-  const { format } = req.query;
+apiRouter.get('/', async (req, res) => {
+  const { format, colorKey } = req.query;
 
-  const color = getColor();
+  const color = await getColor({ key: colorKey });
   const hostname = getHostname();
 
   if (format === 'json') {
@@ -17,6 +18,41 @@ apiRouter.get('/', (req, res) => {
   } else {
     return res.send(`COLOR : ${color}, HOSTNAME : ${hostname}`);
   }
+});
+
+apiRouter.get('/color', async (req, res) => {
+  const colors = await getColors();
+
+  return res.send({ data: colors });
+});
+
+apiRouter.get('/color/:key', async (req, res) => {
+  const { key } = req.params;
+
+  const color = await getColor({ key, strict: true });
+
+  if (!color) {
+    return res.sendStatus(404);
+  } else {
+    return res.send({ data: color });
+  }
+});
+
+apiRouter.post('/color/:key', async (req, res) => {
+  const { key } = req.params;
+  const { value } = req.body;
+
+  await saveColor({ key, value });
+
+  return res.status(201).send({ data: { key, value } });
+});
+
+apiRouter.delete('/color/:key', async (req, res) => {
+  const { key } = req.params;
+
+  await deleteColor({ key });
+
+  return res.sendStatus(204);
 });
 
 module.exports = {
