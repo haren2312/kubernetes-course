e283878abfa71290dae2c39f0e559b6fe117f1f2
Lauro Fialho Müller <contact@lauromueller.com>
2024-09-21 21:47:44 +0200

    feat(color-api): refactor and extract routes

---
+++ containers/color-api/src/routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { getColor, getHostname } = require('../utils');
+
+const apiRouter = express.Router();
+
+apiRouter.get('/', (req, res) => {
+  const { format } = req.query;
+
+  const color = getColor();
+  const hostname = getHostname();
+
+  if (format === 'json') {
+    return res.json({
+      color,
+      hostname,
+    });
+  } else {
+    return res.send(`COLOR : ${color}, HOSTNAME : ${hostname}`);
+  }
+});
+
+module.exports = {
+  apiRouter,
+};
